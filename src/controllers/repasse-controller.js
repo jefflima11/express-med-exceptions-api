0c9f71221cb3eb420d6aeed8e2443638a0f7515b
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2024-12-27 17:32:16 -0300

    Update repasse-controller.js

--- src/controllers/repasse-controller.js
+++ src/controllers/repasse-controller.js
@@ -3,9 +3,10 @@ const connectOracle = require('../config/db-connection');
 const getRepasse = async (req, res) => {
     try {
         const connection = await connectOracle();
-        const result = await connection.execute('SELECT * FROM REPASSE_PRESTADOR WHERE CD_REPASSE = 69');
-        res.status(200).json(result.rows);
+        const result = await connection.execute('SELECT CD_REPASSE,CD_PRESTADOR,VL_REPASSE,CD_PRESTADOR_REPASSE FROM REPASSE_PRESTADOR WHERE ROWNUM <= 10');
+        const columns = result.metaData.map(column => column.name);
         await connection.close();
+        res.status(200).json({columns, data: result.rows});
     } catch (error) {
         res.status(500).json({ message: 'Error on getRepasse', error });
     }
@@ -15,9 +16,14 @@ const getRepasseId = async (req, res) => {
     try {
         const connection = await connectOracle();
         const { id } = req.params;
-        const result = await connection.execute('SELECT * FROM REPASSE_PRESTADOR WHERE CD_REPASSE = :id', [id]);
-        res.status(200).json(result.rows);
+        const result = await connection.execute('SELECT CD_REPASSE,CD_PRESTADOR,VL_REPASSE,CD_PRESTADOR_REPASSE FROM REPASSE_PRESTADOR WHERE CD_REPASSE = :id', [id]);
+        const columns = result.metaData.map(col => col.name);
+        const data = result.rows.map((row) => 
+            Object.fromEntries(row.map((value, index) => [columns[index], value]))
+        );
         await connection.close();
+        res.status(200).json({
+            data});
     } catch (error) {
         res.status(500).json({ message: 'Error on getRepasseId', error });
     }
